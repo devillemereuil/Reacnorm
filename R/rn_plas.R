18adcbf767691712504e7ac791078c2218cb3a2c
Pierre de Villemereuil <pierre.devillemereuil@ephe.psl.eu>
2024-09-04 18:46:37 +0200

    Adding the function rn_phi_decomp(), and add using X in rn_vplas()

--- R/rn_plas.R
+++ R/rn_plas.R
@@ -90,10 +90,10 @@ rn_avg_e <- function(e,
 #       - width: the width over which the integral must be computed
 #                (10 is a generally a good value)
 # Value: The value for V_plas (numeric)
-rn_mean_by_env <- function(env,
-                           shape,
-                           theta,
+rn_mean_by_env <- function(theta,
                            G_theta,
+                           env,
+                           shape,
                            fixed = NULL,
                            width = 10) {
     if (is.null(names(theta))) {
@@ -113,11 +113,13 @@ rn_mean_by_env <- function(env,
 }
 
 ## Compute the plastic variance (V_plas)
-# Args: - env: the environmental values over which the model has been estimated
-#       - shape: the function of the reaction norm fitted by the model
-#       - theta: the average parameters estimated by the model (must be named)
+# Args: - theta: the average parameters estimated by the model (must be named)
 #       - G_theta: the G matrix containing the genetic variance-covariances of
 #                  the parameters
+#       - env: the environmental values over which the model has been estimated
+#       - shape: the function of the reaction norm fitted by the model
+#       - X: the design matrix if the model used was linear (incompatible with "shape")
+#       - S: the error variance-covariance matrix of the linear model if X is used
 #       - fixed: which part of the parameters are fixed (no genetic variation)
 #       - wt_env: weights to use for computing the average over env
 #                 (must be the same length as env)
@@ -125,10 +127,12 @@ rn_mean_by_env <- function(env,
 #       - width: the width over which the integral must be computed
 #                (10 is a generally a good value)
 # Value: The value for V_plas (numeric)
-rn_vplas <- function(env,
-                     shape,
-                     theta,
-                     G_theta,
+rn_vplas <- function(theta,
+                     G_theta = NULL,
+                     env = NULL,
+                     shape = NULL,
+                     X = NULL,
+                     S = NULL,
                      fixed = NULL,
                      wt_env = NULL,
                      correction = FALSE,
@@ -137,31 +141,75 @@ rn_vplas <- function(env,
     if (is.null(names(theta))) {
         stop("The vector theta must be named with the corresponding parameter names")
     }
+    
+    # X is incompatible with shape
+    if (is.null(X) & (is.null(shape) & is.null(env))) {
+        stop("Either the shape and environment or the design matrix X of a reaction norm should be provided")
+    } else if (!is.null(X) & !(is.null(shape) & is.null(env))) {
+        stop("The arguments X and shape cannot be used together.\n If the design matrix is available, it is usually better to use the argument X.")
+    }
+    
+    if (!is.null(X)) {
+        # Check X and theta compatibility
+        if (ncol(X) != length(theta)) {
+            stop("The number of columns in X should be equal to the length of theta.")
+        }
+        # If X is used, it's better to provide S
+        if (is.null(S)) {
+            warning("It is important to provide the error variance-covariance matrix S when using X.")
+            S <- matrix(0, ncol = ncol(X), nrow = ncol(X))
+        }
+    }
+    
+    # G is needed if X is not used
+    if (is.null(G_theta) & is.null(X)) {
+        stop("G_theta is needed if the non-linear approach (using env and shape) is used")
+    }
 
     # Configure variance function
     method <- ifelse(correction, "unbiased", "ML")
-    if (is.null(wt_env)) {wt_env <- rep(1/length(env), length(env))}
+    if (is.null(wt_env) & !is.null(env)) {
+        wt_env <- rep(1/length(env), length(env))
+    } else if (!is.null(X)) {
+        wt_env <- rep(1/nrow(X), nrow(X))
+    }
     func_var <- function(x) {
-        cov.wt(cbind(x), wt = wt_env, method = method)[["cov"]] |>
+        cov.wt(cbind(x), wt = wt_env, method = method)[["cov"]]
+    }
+    
+    # Compute V_Plas
+    if (!is.null(X)) {
+        # Compute the variance-covariance matrix of X
+        cov_X <- func_var(X)
+        # Compute the correcting factor due to the uncertainty
+        var_uncert <- sum(cov_X * S)
+        # Compute V_Plas
+        out <- as.vector(t(theta) %*% cov_X %*% theta - var_uncert)
+        
+    } else {
+        # Compute the average for each environment, then take the variance
+        out <-
+            rn_mean_by_env(theta    = theta,
+                           G_theta  = G_theta,
+                           env      = env,
+                           shape    = shape,
+                           fixed    = fixed,
+                           width    = width) |>
+            func_var() |>
             as.numeric()
     }
-
-    # Compute the average for each environment, then take the variance
-    rn_mean_by_env(env      = env,
-                   shape    = shape,
-                   theta    = theta,
-                   G_theta  = G_theta,
-                   fixed    = fixed,
-                   width    = width) |>
-        func_var()
+    
+    return(out)
 }
 
-## Compute the π-decomposition of V_Plas
-# Args: - env: the environmental values over which the model has been estimated
-#       - shape: the function of the reaction norm fitted by the model
-#       - theta: the average parameters estimated by the model (must be named)
+## Compute the pi-decomposition of V_Plas
+# Args: - theta: the average parameters estimated by the model (must be named)
 #       - G_theta: the G matrix containing the genetic variance-covariances of
 #                  the parameters
+#       - env: the environmental values over which the model has been estimated
+#       - shape: the function of the reaction norm fitted by the model
+#       - X: the design matrix if the model used was linear (incompatible with "shape")
+#       - S: the error variance-covariance matrix of the linear model if X is used
 #       - fixed: which part of the parameters are fixed (no genetic variation)
 #       - wt_env: weights to use for computing the average over env
 #                 (must be the same length as env)
@@ -169,11 +217,11 @@ rn_vplas <- function(env,
 #       - v_plas: optionnaly, provide the value for v_plas if already computed
 #       - width: the width over which the integral must be computed
 #                (10 is a generally a good value)
-# Value: The value for V_plas (numeric)
-rn_pi_decomp <- function(env,
-                         shape,
-                         theta,
+# Value: A data.frame containing V_Plas and the pi-decomposition
+rn_pi_decomp <- function(theta,
                          G_theta,
+                         env,
+                         shape,
                          fixed = NULL,
                          wt_env = NULL,
                          correction = FALSE,
@@ -193,7 +241,7 @@ rn_pi_decomp <- function(env,
 
     # Configure variance function
     method <- ifelse(correction, "unbiased", "ML")
-    if (is.null(wt_env)) {wt_env <- rep(1/length(env), length(env))}
+    if (is.null(wt_env)) { wt_env <- rep(1/length(env), length(env)) }
     func_var <- function(x) {
         cov.wt(cbind(x), wt = wt_env, method = method)[["cov"]] |>
             as.numeric()
@@ -202,11 +250,12 @@ rn_pi_decomp <- function(env,
     # Compute V_plas
     if (is.na(v_plas)) {
         v_plas <-
-            rn_vplas(env        = env,
-                     shape      = shape,
-                     theta      = theta,
+            rn_vplas(theta      = theta,
                      G_theta    = G_theta,
+                     env        = env,
+                     shape      = shape,
                      fixed      = fixed,
+                     wt_env     = wt_env,
                      correction = correction,
                      width      = width)
     }
@@ -247,4 +296,92 @@ rn_pi_decomp <- function(env,
     data.frame(V_Plas = v_plas,
                Pi_Sl  = var_sl / v_plas,
                Pi_Cv  = var_cv / v_plas)
+}
+
+## Compute the phi-decomposition of V_Plas
+# Args: - theta: the average parameters estimated by the model (must be named)
+#       - X: the design matrix containing the environmental values for each power
+#            of the environment
+#       - S: the error variance-covariance matrix of the estimates parameters of the linear model
+#       - wt_env: weights to use for computing the average over env
+#                 (must be the same length as env)
+#       - correction: should the Bessel correction be used or not?
+#       - v_plas: optionnaly, provide the value for v_plas if already computed
+# Value: A data.frame containing V_Plas and the pi-decomposition
+rn_phi_decomp <- function(theta,
+                          X,
+                          S = NULL,
+                          wt_env = NULL,
+                          correction = FALSE,
+                          v_plas = NA) {
+    
+    # If X is used, it's better to provide S
+    if (!is.null(X) & is.null(S)) {
+        warning("It is important to provide the error variance-covariance matrix S when using X.")
+        S <- matrix(0, ncol = ncol(X), nrow = ncol(X))
+    }
+    
+    #  Checking that dimensions are correct
+    if (ncol (X) != nrow(S) | ncol(X) != ncol(S)) {
+        print("Incompatible dimensions between the design matrix X and the error matrix S")
+    }
+    
+    # Configure variance function
+    method <- ifelse(correction, "unbiased", "ML")
+    if (is.null(wt_env)) { wt_env <- rep(1/nrow(X), nrow(X)) }
+    func_var <- function(x) {
+        cov.wt(cbind(x), wt = wt_env, method = method)[["cov"]]
+    }
+    
+    # Compute the variance-covariance matrix of X
+    cov_X <- func_var(X)
+    
+    if (is.na(v_plas)) { 
+        # Compute the correcting factor due to the uncertainty
+        var_uncert <- sum(cov_X * S)
+        
+        # Compute V_Plas
+        v_plas <- as.vector(t(theta) %*% cov_X %*% theta - var_uncert)
+    }
+    names(v_plas) <- "V_Plas"
+    
+    # Computing the variance-linked components of the phi-decomposition
+    phi_i <- diag(cov_X) * theta^2 - diag(cov_X * S)
+    if (phi_i[1] != 0) {
+        warning("The intercept-level phi_0 was not 0, did you include the intercept in the design matrix X?")
+        intercept <- FALSE
+    } else {
+        # Removing the intercept-level phi_0
+        phi_i <- phi_i[-1]
+        intercept <- TRUE
+    }
+    names(phi_i) <- paste0("Phi_", 1:length(phi_i))
+    
+    # Computing the covariance-linked components of the phi-decomposition
+    if (intercept) {
+        cov_X_trim <- cov_X[-1, -1]
+        S_trim     <- S[-1, -1]
+        M_ij <- matrix(paste(rep(1:length(phi_i), length(phi_i)),
+                             rep(1:length(phi_i), each = length(phi_i)),
+                             sep = "_"),
+                       ncol = length(phi_i), nrow = length(phi_i))
+    } else {
+        cov_X_trim <- cov_X
+        S_trim     <- S
+        M_ij <- matrix(paste(rep(0:(length(phi_i)-1), length(phi_i)),
+                             rep(0:(length(phi_i)-1), each = length(phi_i)),
+                             sep = "_"),
+                       ncol = length(phi_i), nrow = length(phi_i))
+    }
+    phi_ij <- 2 * cov_X_trim[upper.tri(cov_X_trim)] - 
+              cov_X_trim[upper.tri(cov_X_trim)] * S_trim[upper.tri(S_trim)]
+    names(phi_ij) <- paste0("Phi_", M_ij[upper.tri(M_ij)])
+    
+    # Formatting the output
+    out <- 
+        c(v_plas, phi_i / v_plas, phi_ij / v_plas) |>
+        as.list() |>
+        as.data.frame()
+    
+    return(out)
 }
